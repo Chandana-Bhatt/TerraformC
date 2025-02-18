4e7b15958ccd38781e81b0f10b81388e06bef464
Chandana Bhatt <chandana18bhatt96@gmail.com>
2025-02-18 13:39:13 -0800

    created remote backend and provider files

---
+++ variables.tf
@@ -0,0 +1,17 @@
+# Environment variables
+variable "aws_region" {
+    description = "AWS region to create resources in"
+    type = string
+ 
+}
+
+variable "project_name" {
+    description = "Name of the project"
+    type = string
+  
+}
+
+variable "environment" {
+    description = "Environment to deploy the resources"
+    type = string
+}
